ef8283d3b39f729919748382870b9b9411bc609c
Victor Hugo Forbes <vhforbes@MacBook-Air-de-Victor.local>
2023-05-08 17:08:24 -0300

    feat: paginated buttons

--- components/shared/pageButtons.tsx
+++ components/shared/pageButtons.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/react/24/outline';
 
 interface PagesInfo {
   totalPages: number;
@@ -11,18 +12,52 @@ const PageButtonsComponent = ({
   changePage,
   currentPage,
 }: PagesInfo) => {
-  const buttons = [];
+  const [currentButtonsPage, setCurrentButtonsPage] = useState(1);
+
+  const itemsPerPage = 4;
+
+  const buttons: number[] = [];
 
   for (let i = 1; i <= totalPages; i += 1) {
     buttons.push(i);
   }
 
+  const getButtonsForCurrentPage = (buttonsArray: number[]) => {
+    const startIndex = (currentButtonsPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return buttonsArray.slice(startIndex, endIndex);
+  };
+
+  const handleNextPageClick = () => {
+    const lastNumberFromFullArray = buttons[buttons.length - 1];
+    const lastNumberFromPaginatedArray =
+      getButtonsForCurrentPage(buttons)[
+        getButtonsForCurrentPage(buttons).length - 1
+      ];
+
+    if (lastNumberFromFullArray !== lastNumberFromPaginatedArray) {
+      setCurrentButtonsPage(currentButtonsPage + 1);
+    }
+  };
+
+  const handlePreviousPageClick = () => {
+    if (currentButtonsPage !== 1) {
+      setCurrentButtonsPage(currentButtonsPage - 1);
+    }
+  };
+
   return (
     <div>
-      {buttons.map(pageNumber => (
+      <button type="button" className="hover:cursor-pointer">
+        <ChevronLeftIcon
+          onClick={handlePreviousPageClick}
+          className="h-5 w-4 cursor-pointer"
+        />
+      </button>
+      {getButtonsForCurrentPage(buttons).map(pageNumber => (
         <button
-          key={pageNumber}
           type="button"
+          key={pageNumber}
           className={`btn btn-secondary m-1 border-2 border-neutral border-opacity-30 ${
             pageNumber === currentPage ? 'bg-primary btn-disabled' : null
           }`}
@@ -31,6 +66,14 @@ const PageButtonsComponent = ({
           {pageNumber}
         </button>
       ))}
+
+      <button
+        type="button"
+        onClick={handleNextPageClick}
+        className="hover:cursor-pointer"
+      >
+        <ChevronRightIcon className="h-5 w-4 cursor-pointer" />
+      </button>
     </div>
   );
 };
