a30fe361f0aa84402ca638e6a9a2640501256534
Victor Forbes <vhforbes@gmail.com>
2023-07-26 11:22:36 -0300

    feat: video

--- pages/sign-in/index.tsx
+++ pages/sign-in/index.tsx
@@ -70,7 +70,7 @@ const SignIn: NextPage = () => {
           <iframe
             width="560"
             height="315"
-            src={`https://www.youtube.com/embed/zPvkBXeObP0`}
+            src="https://www.youtube.com/embed/zPvkBXeObP0"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
